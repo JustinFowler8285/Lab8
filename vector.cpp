b30265b99efd458083f45247d2552d1a02a5870f
JustinFowler8285 <008285349@coyote.csusb.edu>
2024-04-09 11:10:47 -0700

    addedd

---
+++ vector.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/**
+ * @brief prints the elements in the vector anf thier memory locations
+ * 
+ * @param v - vector of intergers
+*/
+void printMemVec(...){
+
+}
+/**
+ *@brief increments all of the elements in v by 10
+
+ @param v - address to a vector of intergers
+*/
+void inceVecBy10(...){
+
+}
+
+int main() {
+    // create a constant integer called SIZE that is of value 5
+
+    // create a vector of integers called vec that can hold up to 5 elements
+
+
+    //use a for loop to populate vec with the values 100 to 104
+
+
+    printf("Before Increment------------\n");
+    //call printMemvec(...) on vec
+
+    //call incBy10(...) on vec
+
+
+    printf("After Increment------------\n");
+    //call printMemVec(...) on vec again to view the changes
+
+
+    // remove last element of vec
+
+
+
+    printf("After Pop------------\n");
+    //call printMemVec(...) on vec again to view the changes
+
+
+    // append 101 and 102 at the end of vec
+
+
+    printf("After Push------------\n");
+    //call printMemvec(...) on vec again to view the changes
+
+    return 0;
+}
